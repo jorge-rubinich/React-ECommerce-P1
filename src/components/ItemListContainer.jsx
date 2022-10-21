ca6e93ed0472863372f331f57e482800c218820d
Bochincito <jr@bitbyte.com.ar>
2022-10-21 01:22:10 -0300

    Se pasa render de cards a ItemList

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,38 +1,21 @@
 import React from 'react'
 import { useState, useEffect } from "react";
+import ItemList from './ItemList';
 
 const ItemListContainer = (props) => {
     const [items, setItems] = useState([]);
-
     useEffect(() => {
     console.log("useEffect");
     fetch("./productos.json")
         .then(response =>response.json())
-        .then(data => {
-            console.log(data);
-            setItems(data);
-        })
+        .then(data => {  
+            setItems(data);}
+        )
         .catch(error => { alert("Diantres!!  Algo ha fallado..  Error: "+error+""); })
-    }, []);
+    },[]);
 
     return (
-        <div id="container">
-        {items.map((producto) => {
-            return (
-                <div key={producto.codigo}>
-                    <img
-                        src={producto.imagen}
-                        width="200px"
-                        alt={producto.nombre}
-                    />
-                    <article>
-                        <h2>{producto.nombre}</h2>
-                        <h3>${producto.precio}.-</h3>
-                    </article>
-                </div>
-            );
-        })}
-    </div>
+        <ItemList itemList={items}/>
       )
 }
 
